6b3eab471f8aaef7760b77036ce58fe42aeb5117
Ronskie-phams <ron.pama@rcsd.ca>
2024-02-29 11:50:29 -0600

    created project

---
+++ New folder/sketch.js
@@ -0,0 +1,23 @@
+// A1 character
+// Ron Pama
+// 2/29/2024
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  fill("brown");
+
+  circle(width/2,height/2,100);
+  
+
+  
+
+
+
