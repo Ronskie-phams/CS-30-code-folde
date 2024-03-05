f3b33bbae901931697baa928ac690e493781b12e
Ronskie-phams <ron.pama@rcsd.ca>
2024-03-05 12:03:18 -0600

    Update sketch.js

--- New folder/sketch.js
+++ New folder/sketch.js
@@ -6,18 +6,48 @@
 // - describe what you did to take this project "above and beyond"
 
 
+
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  
 }
+
+ 
 
 function draw() {
   background(220);
-  fill("brown");
 
-  circle(width/2,height/2,100);
-  
 
   
+    fill("blue")
+  
+  
+  circle(width/2,380,50); //Head
+  
+  
+ 
+   
+  rect(405,410,60,100);// body
+   
 
+   
+  rect(370,410,30,100);//left arm
+   
 
-
+  rect(470,410,30,100);//right arm
+  
+  
+  
+  rect(440,515,25,100); //right leg
+  
+ 
+  
+  rect(405,515,25,100);// left leg
+  
+  
+ 
+ 
+  
+}
