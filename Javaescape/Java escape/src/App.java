28dd1bac6fc242e3d584bdac8cacb3c6159073b7
Ronskie-phams <ron.pama@rcsd.ca>
2024-03-19 12:08:28 -0600

    A5 java escape

--- Javaescape/Java escape/src/App.java
+++ Javaescape/Java escape/src/App.java
@@ -1,12 +1,10 @@
-// Program/Assignment name: (The name you gave the program)
+/// Program/Assignment name: A5 assignment
 
-//Student name: (Your name)
+//Student name: Ron Pama
 
-//Date: (The date you completed the program)
+//Date: 3/19/24
 
-//Program Description:
-
-
+//Program Description: escape sequence, deletes text and spaces and creating sentence in a new line instead of putting it all together.
 
 
 
@@ -15,6 +13,9 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
+        
+        
+        
         System.out.println("My Name is  \bRon and my favourite food is  \bChicken. \nI wanna try other foods too.");
     }
 }
