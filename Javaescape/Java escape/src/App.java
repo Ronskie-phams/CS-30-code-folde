2a5364e2123cb36e13d4c89b87a7e33d106fc0ed
Ronskie-phams <ron.pama@rcsd.ca>
2024-03-19 11:46:35 -0600

    A5 Java escape seq

---
+++ Javaescape/Java escape/src/App.java
@@ -0,0 +1,20 @@
+// Program/Assignment name: (The name you gave the program)
+
+//Student name: (Your name)
+
+//Date: (The date you completed the program)
+
+//Program Description:
+
+
+
+
+
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("My Name is  \bRon and my favourite food is  \bChicken. \nI wanna try other foods too.");
+    }
+}
