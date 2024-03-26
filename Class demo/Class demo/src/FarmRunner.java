87102669c8a43e259ef3cde47972d5fa69c12160
Ronskie-phams <ron.pama@rcsd.ca>
2024-03-26 11:52:46 -0600

    Class demo

---
+++ Class demo/Class demo/src/FarmRunner.java
@@ -0,0 +1,52 @@
+public class FarmRunner {
+    public static void main(String[] args) throws Exception {
+       
+
+      
+      
+      
+      Turkey bird = new Turkey();
+      Pig pig = new Pig();
+
+
+
+
+
+      bird.speak();
+      bird.sayName();
+      pig.speak();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Class demo/Class demo/src/Turkey.java
@@ -0,0 +1,15 @@
+public class Turkey {
+    
+
+    public void speak() {
+       System.out.println("gobble - gobble");
+    }
+
+    public void sayName(){
+
+
+          System.out.println("big bird");
+    speak();
+    }
+
+}
