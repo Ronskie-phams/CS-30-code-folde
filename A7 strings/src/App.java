cb338cacb3414d0c9655b101c47dc937cac95024
Ronskie-phams <ron.pama@rcsd.ca>
2024-03-28 11:55:39 -0600

    A7 strings

---
+++ A7 strings/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       String message1= "I am very happy";
+       
+
+       
+       
+       System.out.println("I am" +  message1.substring(9));
+       
+       String message2 = "That was great - lol";
+       
+
+
+       System.out.println("That was great - " + message2.substring(20) + "laugh out loud");
+       
+       
+
+    }
+}
