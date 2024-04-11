6035dece1d2399c25368885117ae28c32031329f
Ronskie-phams <ron.pama@rcsd.ca>
2024-04-11 11:25:52 -0600

    bigorsmallA9

--- Big or small A9/src/BigorsmallA9.java
+++ Big or small A9/src/BigorsmallA9.java
@@ -1,36 +1,31 @@
+
+
+
+
+
 public class BigorsmallA9 {
-    public static void main(String[] args) throws Exception {
-        
-     int x= 10;
-     int y = 20;
-     int r = 0;
-     int z = 1;
-     int k = 3;
-     int l = 5;
-     int p = 55342;
-     int i = 323;
+    public static void check(int a, int b) 
+    {
 
 
+      if (a>b  ){
 
-     
-     
-    if ((y>10 && x<20)){
-        System.out.println("yes");
-     }
-    
-    else if (x==20 || y == 10 ){
-        System.out.println("equals");
-    }
-    else{
+
+       System.out.println("yes");
+      }
+
+
+       
+
+     else if (b<a  ){
+
         System.out.println("no");
-    } 
-     }
-        
 
-//   int x = 2;
-//   int y= 3 ; 
-//   System.out.println((x>3&&y<2));
+       }
 
+     else  {
+        System.out.println("equals");
+       }
 
 
 
@@ -38,25 +33,20 @@ public class BigorsmallA9 {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    
+    
+    
+    
     }
+    
+    
+        
+}
+
+  
+    
+
+
+   
 
---
+++ Big or small A9/src/BigorsmallrunnerA9.java
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+public class BigorsmallrunnerA9 
+{
+    public static void main(String args[]) throws Exception
+
+    
+    {
+
+        BigorsmallA9.check( 10, 20);		
+		BigorsmallA9.check( 20, 10);	
+		BigorsmallA9.check( 20, 20);
+		BigorsmallA9.check( 10, 10);	
+		BigorsmallA9.check( 0, 1);	
+		BigorsmallA9.check( 1, 0 );	
+		BigorsmallA9.check( 3, 5);	
+		BigorsmallA9.check( 5, 3 );	
+		BigorsmallA9.check( 5342, 323 );		
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+}
