7c924e376386997008915e7e1dec7e7f519f0517
Ronskie-phams <ron.pama@rcsd.ca>
2024-04-15 12:08:10 -0600

    set nums

---
+++ setnums/src/Sumrunner.java
@@ -0,0 +1,63 @@
+
+
+
+
+
+
+
+public class Sumrunner {
+
+
+    private double one,two,sum;
+
+
+    public void setNums(double num1, double num2)
+    {
+       one = num1;
+       two = num2;
+       
+
+
+    }
+    
+    public void sum()
+    {
+     
+        sum = one + two ;
+      
+
+
+
+
+    }
+
+    public void print()
+    {
+
+       System.out.println(sum);
+     
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ setnums/src/Sums.java
@@ -0,0 +1,16 @@
+public class Sums {
+    public static void main(String[] args) throws Exception {
+       Sumrunner test = new Sumrunner();
+
+       test.setNums(2,6);
+       test.sum();
+       test.print();
+
+
+ 
+
+       // add more test cases
+
+
+    }
+}
