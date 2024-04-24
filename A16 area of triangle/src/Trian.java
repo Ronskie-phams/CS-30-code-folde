e40e122b968234c45c2253ec1d2e7628e18d3614
Ronskie-phams <ron.pama@rcsd.ca>
2024-04-24 11:47:39 -0600

    4/24/24

--- A16 area of triangle/src/Trian.java
+++ A16 area of triangle/src/Trian.java
@@ -3,22 +3,23 @@ import java.util.Scanner;
 import java.lang.Math.*;
 
 public class Trian
-{
-    private double setA,setB, setC;
+{    
+	// set global variables
+    private double setA,setB, setC, stA, stB, stC;
 	private double sideA, sideB, sideC;
-	private double perimeter, per;
-	private double theArea, area2;
+	private double perimeter1, per2, per3;
+	private double theArea, area2, area3;
 
 	public Trian()
 	{
 	   setSides(0,0,0);
-	   perimeter=0;
+	   perimeter1=0;
 	   theArea=0;
 	}
 
-	public Trian(int a, int b, int c)
+	public Trian(int a, int b, int c) // first triangle input
 	{
-      sideA= a;
+      sideA = a;
       sideB = b;
       sideC = c;
 
@@ -27,32 +28,49 @@ public class Trian
 	}
 
 
-	public void setSides(int a, int b, int c)
+	public void setSides(int a, int b, int c) //second triangle input
 	{
         setA = a;
         setB = b;
         setC = c;
 
+
 	}
 
-	public void calcPerimeter( )
+	public void setSides2( int a, int b, int c)// third trianlge input
+	
 	{
-        perimeter = sideA + sideB + sideC ;
-        per = setA + setB + setC;
+       stA = a;
+	   stB = b;
+	   stC = c;
+
+
+	
+	}
+
+	public void calcPerimeter( )// perimeters
+	{
+        perimeter1 = sideA + sideB + sideC ; // for first triangle
+        per2 = setA + setB + setC;// second triangle
+		per3 = stA + stB + stC ;// third trianlge
+
         
 
      
 
 	}
 
-	public void calcArea( )
+	public void calcArea( )// area
 	{   
         
-        double s = perimeter/2;
-	    theArea = Math.sqrt(s*(s-sideA)*(s-sideB)*(s-sideC));
+        double s = perimeter1/2;
+	    theArea = Math.sqrt(s*(s-sideA)*(s-sideB)*(s-sideC));// area calc for triangle 1
 
-        double r = per/2;
-        area2 = Math.sqrt(r*(r-setA)*(r-setB)*(r-setC));
+        double r = per2/2;
+        area2 = Math.sqrt(r*(r-setA)*(r-setB)*(r-setC)); // area calc for triangle 2
+
+		double a = per3/2;
+		area3 = Math.sqrt(a*(a-stA)*(a-stB)*(a-stC)); // area calc for triangle 3
 
 
         
@@ -61,13 +79,30 @@ public class Trian
 
 	}
 
-	public void print( )
+	public void printTrian1( )
 	{
      System.out.println("\n\n");
-     System.out.println("The area is " + theArea);
-     System.out.println("\n\n");
-     System.out.println("The area is " + area2);
+     System.out.println("The area is " + theArea); // prints triangle 1 answer
+     
+     
+	}
+
+
+	public void printTrian2()
+	{
+		System.out.println("\n\n");
+		System.out.println("The area is " + area2);// prints triangle 2 answer
+	}
+
+
+
+	public void printTrian3()
+	{
+		System.out.println("\n\n");
+		System.out.println("The area is " + area3);// prints triangle 3 answer
 	}
 }
+
+
     
 
--- A16 area of triangle/src/Trianrunner.java
+++ A16 area of triangle/src/Trianrunner.java
@@ -17,12 +17,14 @@ public class Trianrunner  //this class is used to test Triangle
 
 		System.out.print("Enter side C ::  ");
 		int c = keyboard.nextInt();
+        
+		System.out.println(a+ "\t"+  b + "\t" + c);
+		
 
-
-		Trian test = new Trian(a, b, c);
+		Trian test = new Trian(a, b, c);// running the codes
 		test.calcPerimeter();
 		test.calcArea();
-		test.print();
+		test.printTrian1();
 
 
 		//ask for user input
@@ -34,15 +36,39 @@ public class Trianrunner  //this class is used to test Triangle
 
 		System.out.print("Enter side C ::  ");
 		c = keyboard.nextInt();
-
-
+          
+		//prints the set numbers
+        System.out.println(a+ "\t"+  b + "\t" + c);
+		
 		test.setSides(a,b,c);
 		test.calcPerimeter();
 		test.calcArea();
-		test.print();
+		test.printTrian2();
 
 
-		//add one more input section
+		
+		// new added input
+		System.out.print("Enter side A:  ");
+		a = keyboard.nextInt();
+
+		System.out.print("Enter side B:  ");
+		b = keyboard.nextInt();
+
+		System.out.print("Enter side C:  ");
+		c = keyboard.nextInt();
+
+        System.out.println(a+ "\t"+  b + "\t" + c);// prints the chosen numbers
+		
+		test.setSides2(a,b,c);
+		test.calcPerimeter();
+		test.calcArea();
+		test.printTrian3();
+		
+		
+		
+		
+		
+		
 
 
 
