857f16575ddc290a209561c8eca13e92a763de8d
Ronskie-phams <ron.pama@rcsd.ca>
2024-04-29 12:07:01 -0600

    4/29/24

---
+++ A22 leap year/src/Leaprunner.java
@@ -0,0 +1,28 @@
+
+
+import static java.lang.System.*; 
+import java.util.Scanner;
+
+
+
+public class Leaprunner {
+    public static void main( String args[] )
+	{
+		Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter a number::"    );
+        int yr = keyboard.nextInt();
+
+        if( leap.isLeapYear(yr)) System.out.println(yr + " is a leap year"); 
+         else System.out.println(yr + " is not a leap year");
+
+
+
+       
+        
+
+        
+		
+					
+	}
+}
+   
---
+++ A22 leap year/src/leap.java
@@ -0,0 +1,33 @@
+
+import static java.lang.System.*; 
+import java.util.Scanner;
+
+
+
+public class leap {
+
+
+	public static boolean isLeapYear(int yr) {
+
+    {
+        if (( yr % 4 == 0 && yr % 100 !=0) || (yr % 400 == 0 ))
+        
+        {
+
+            return true;
+        }
+        else 
+        {
+                return false;
+        }
+        
+        
+       
+   
+    
+  
+
+}
+    
+}
+} 
