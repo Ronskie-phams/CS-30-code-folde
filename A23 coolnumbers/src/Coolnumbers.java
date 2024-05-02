abd42d180b916cc0350d09f1bb8e9d8239f44684
Ronskie-phams <ron.pama@rcsd.ca>
2024-05-02 12:09:31 -0600

    5/2/24

---
+++ A23 coolnumbers/src/Coolnumbers.java
@@ -0,0 +1,43 @@
+import static java.lang.System.*;
+
+
+
+
+public class Coolnumbers 
+
+    
+{
+	/*
+	 *method isCoolNumber will return true if
+	 *	num % 3-6 all have a remainder of 1
+	 *it will return false otherwize
+	 */
+	public static boolean isCoolNumber(  int num )
+
+	{
+
+       
+		 return num % 3 ==1 && num % 4 ==1 && num % 5 ==1 && num%6 ==1 ;
+        
+         
+         
+	} 
+    
+    
+	
+		/*
+	 *method countCoolNumbers will return the count
+	 *of the coolNumbers between 6 and stop
+	 */
+
+     
+	public static int countCoolNumbers( int stop )
+
+	{
+       
+    
+        
+		return ;
+        
+	}
+}
---
+++ A23 coolnumbers/src/Coolnumrunner.java
@@ -0,0 +1,19 @@
+
+
+
+import static java.lang.System.*;
+
+import java.util.Scanner;
+
+
+public class Coolnumrunner 
+{
+	public static void main( String[] args )
+	{
+		System.out.println( Coolnumbers.countCoolNumbers(250) + " cool numbers between 6 - " + 250);
+		//add more test cases
+        Scanner check = new Scanner(System.in);
+        
+	}
+}
+    
