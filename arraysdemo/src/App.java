697eb7c412258e5a585a8b81a78bc5975887aa23
Ronskie-phams <ron.pama@rcsd.ca>
2024-05-10 12:12:10 -0600

    5/10/24

--- arraysdemo/src/App.java
+++ arraysdemo/src/App.java
@@ -1,5 +1,50 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+          
+        // // Declaring Arrays
+        // int[] highscore = null;
+        // String[] names = null;
+
+        // // Create the array
+        // highscore = new int[5];
+        // names = new String[5];
+
+        // // Set initial values invidiually
+        // highscore[0] = 99;
+        // names[0] = "george";
+
+        // Declare, Create, and intialize at the same time
+        int[] highscore = {99,98,98,88,68};
+        String[] names = {"g","fred","wolf","fox","sutter"};
+
+        // System.out.println("The length is: " + names.length);
+
+
+
+        // python reference
+
+        // for item in names:
+          // do something 
+
+          // for item in highscore:
+          // do something 
+           
+
+                  // default for loop to go through array backwards
+        //   for (int i = highscore.length-1 ; i >=0 ; i--){
+        //       System.out.println(" Item number" + i + "is"+ highscore[i]);
+        //       System.out.println("Name #" + i+ "is"+ names[i] + "\n");
+
+
+         // default for loop ti access to any array
+
+         for (int i = 0; i<highscore.length; i++){
+            System.out.println(" Item number" + i + "is"+ highscore[i]);
+            System.out.println("Name #" + i+ "is"+ names[i] + "\n");
+
+         }
+
+          
+        } 
     }
-}
+
