d0335064396e4338e2362b3e25859b2ad236e87a
Ronskie-phams <ron.pama@rcsd.ca>
2024-05-24 11:05:11 -0600

    5/24/24

--- A31 array fun house/src/funhouse.java
+++ A31 array fun house/src/funhouse.java
@@ -9,6 +9,7 @@
 import java.lang.System;
 import java.lang.Math;
 
+
 public class funhouse
 {
 	//instance variables and constructors could be used, but are not really needed
@@ -16,17 +17,16 @@ public class funhouse
 	//getSum() will return the sum of the numbers from start to stop, not including stop
 	public static int getSum(int[] numArray, int start, int stop)
 	{
-		int sum = 0;
-        for (int  i = start; i < numArray[numArray.length-stop] ; i++ ){
-            
-			sum += numArray[i] ;
-            
-		   
+	  int sum = 0;
 
-        }
+	  for( int i = start; i<= stop; i++){ // loops that i = the start value and it stops at the value of "stop" and adds them
+		sum +=numArray[i]; // adds the whatever the sum is to the variable "sum"
+	  }
+ 	
+		return sum; // returns sum
 
-
-	   return sum ;
+		
+		
 	}
 
 	//getCount() will return number of times val is present
@@ -34,27 +34,25 @@ public class funhouse
 	{
 	  int count = 0;
       for (int number:numArray){
-		if (number == 4){
-			count ++;
+		if (number == val){// if number matches int val
+			count++;// it counts how many int val are in the array
 		}
 	  }
-		return count;
+		return count;// returns count 
 	}
 
 	public static int[] removeVal(int[] numArray, int val)
 	{ 
-	   
-	   
+	   int [] newarray = new int[numArray.length - getCount(numArray,val)]; // creates new array that counts a certain value of the array
+	   int pos = 0; // sets position of each array
 		
-		for (int i = 0 ; i<numArray.length; i++){
-		if ( numArray[i] ==  4){
-			  numArray[i] = 0 ;
+		for (int num : numArray){// it loops each of the element and specifies the array numbers to iterate over, it is an enhanced loop from the traditional loop method
+		if ( num !=  val){ // if number in each element does not equal to the designated number of val
+		    newarray[pos++] = num ; // places value of num to the  new array that was made and it will be placed at the "pos" position and will increment pos so that the next value will be placed in different positions of newarray.
 			 
 		}
 	  }
 	  
-
-
-		return numArray;
+		return newarray;
 	}
 }
--- A31 array fun house/src/funhouserunner.java
+++ A31 array fun house/src/funhouserunner.java
@@ -15,8 +15,8 @@ public class funhouserunner
 		System.out.println("# of 4s  =  " + funhouse.getCount(one,4));
 		System.out.println("# of 9s  =  " + funhouse.getCount(one,9));
         
-		int [] newArray = funhouse.removeVal(one,4);
-	    System.out.println("Array after removing 4:"+ Arrays.toString(newArray));
+		int [] newArray = funhouse.removeVal(one,4);// new list 
+	    System.out.println("Array after removing 4:"+ Arrays.toString(newArray));// prints new list 
 		
 		
 		
@@ -24,6 +24,17 @@ public class funhouserunner
 		
 		int[] two = {4,2,3,4,6,7,8,9,0,10,0,1,7,6,5,3,2,9,9,8,7};
 
+
+		System.out.println(Arrays.toString(two));
+		System.out.println("sum of spots 3-6  =  " + funhouse.getSum(two,3,6));// calls the function from funhouse and sets the array,start,and stop
+		System.out.println("sum of spots 2-9  =  " + funhouse.getSum(two,2,9));
+		System.out.println("# of 4s  =  " + funhouse.getCount(two,4));// sets the value to be counted from the array two
+		System.out.println("# of 9s  =  " + funhouse.getCount(two,9));
+        
+		int [] newList = funhouse.removeVal(two,9); // creates the new list and removes the value of the of 9 in the array
+	    System.out.println("Array after removing 9:"+ Arrays.toString(newList)); // prints the new list
+		
+		
 		//add test cases
 
 
